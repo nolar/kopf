5e8248b470deb0586e033fabe775e733ba88b3f1
Sergey Vasilyev <sergey.vasilyev@zalando.de>
2019-03-27 15:19:53 +0100

    Add README to the PyPI page, plus some other metainfo

--- setup.py
+++ setup.py
@@ -1,13 +1,31 @@
+import os.path
+
 from setuptools import setup, find_packages
+
+LONG_DESCRIPTION = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+DESCRIPTION = LONG_DESCRIPTION.splitlines()[0].lstrip('#').strip()
+PROJECT_URLS = {
+    'Documentation': 'https://github.com/zalando-incubator/kopf/blob/master/README.md',
+    'Bug Tracker': 'https://github.com/zalando-incubator/kopf/issues',
+    'Source Code': 'https://github.com/zalando-incubator/kopf',
+}
 
 setup(
     name='kopf',
     use_scm_version=True,
-    url='https://pypi.org/project/kopf/',
 
+    url=PROJECT_URLS['Source Code'],
+    project_urls=PROJECT_URLS,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
     author='Sergey Vasilyev',
     author_email='sergey.vasilyev@zalando.de',
+    maintainer='Sergey Vasilyev, Chiara Mezzavilla',
+    maintainer_email='sergey.vasilyev@zalando.de, chiara.mezzavilla@zalando.de',
+    keywords=['kubernetes', 'operator', 'framework', 'python', 'k8s'],
+    license='MIT',
 
+    zip_safe=True,
     packages=find_packages(),
     include_package_data=True,
     entry_points={
