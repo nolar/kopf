87aeb0344528b8e89fcb4e902fef68fb0818623f
Sergey Vasilyev <sergey.vasilyev@zalando.de>
2019-04-10 12:18:01 +0200

    Replace StopIteration with StopStreaming for async stream generators

--- kopf/reactor/watching.py
+++ kopf/reactor/watching.py
@@ -5,8 +5,17 @@ Kubernetes client's watching streams are synchronous. To make them asynchronous,
 we put them into a `concurrent.futures.ThreadPoolExecutor`,
 and yield from there asynchronously.
 
+However, async/await coroutines misbehave with `StopIteration` exceptions
+as raise by the `next` method: see `PEP-479`_.
+
+As a workaround, we replace `StopIteration` with our custom `StopStreaming`
+inherited from `RuntimeError` (as suggested by `PEP-479`_),
+and re-implement the generators to make them async.
+
 All of this is a workaround for the standard Kubernetes client's limitations.
 They are not needed if the client library is natively asynchronous.
+
+.. _PEP-479: https://www.python.org/dev/peps/pep-0479/
 """
 
 import asyncio
@@ -15,10 +24,30 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+class StopStreaming(RuntimeError):
+    """
+    Raised when the watch-stream generator ends streaming.
+    Replaces `StopIteration`.
+    """
+
+
+def streaming_next(src):
+    """
+    Same as `next`, but replaces the `StopIteration` with `StopStreaming`.
+    """
+    try:
+        return next(src)
+    except StopIteration as e:
+        raise StopStreaming(str(e))
+
+
 async def streaming_aiter(src, loop=None, executor=None):
     """
     Same as `iter`, but asynchronous and stops on `StopStreaming`, not on `StopIteration`.
     """
     loop = loop if loop is not None else asyncio.get_event_loop()
     while True:
-        yield await loop.run_in_executor(executor, next, src)
+        try:
+            yield await loop.run_in_executor(executor, streaming_next, src)
+        except StopStreaming:
+            return
---
+++ tests/test_streaming.py
@@ -0,0 +1,77 @@
+import collections.abc
+
+import pytest
+
+from kopf.reactor.watching import StopStreaming, streaming_next, streaming_aiter
+
+
+async def test_streaming_next_never_ends_with_stopiteration():
+    lst = []
+    src = iter(lst)
+
+    with pytest.raises(StopStreaming) as e:
+        streaming_next(src)
+
+    assert not isinstance(e, StopIteration)
+    assert not isinstance(e, StopAsyncIteration)
+
+
+async def test_streaming_next_yields_and_ends():
+    lst = [1, 2, 3]
+    src = iter(lst)
+
+    val1 = streaming_next(src)
+    val2 = streaming_next(src)
+    val3 = streaming_next(src)
+    assert val1 == 1
+    assert val2 == 2
+    assert val3 == 3
+
+    with pytest.raises(StopStreaming):
+        streaming_next(src)
+
+
+async def test_streaming_iterator_with_regular_next_yields_and_ends():
+    lst = [1, 2, 3]
+    src = iter(lst)
+
+    itr = streaming_aiter(src)
+    assert isinstance(itr, collections.abc.AsyncIterator)
+    assert isinstance(itr, collections.abc.AsyncGenerator)
+
+    val1 = next(src)
+    val2 = next(src)
+    val3 = next(src)
+    assert val1 == 1
+    assert val2 == 2
+    assert val3 == 3
+
+    with pytest.raises(StopIteration):
+        next(src)
+
+
+async def test_streaming_iterator_with_asyncfor_works():
+    lst = [1, 2, 3]
+    src = iter(lst)
+
+    itr = streaming_aiter(src)
+    assert isinstance(itr, collections.abc.AsyncIterator)
+    assert isinstance(itr, collections.abc.AsyncGenerator)
+
+    vals = []
+    async for val in itr:
+        vals.append(val)
+    assert vals == lst
+
+
+async def test_streaming_iterator_with_syncfor_fails():
+    lst = [1, 2, 3]
+    src = iter(lst)
+
+    itr = streaming_aiter(src)
+    assert isinstance(itr, collections.abc.AsyncIterator)
+    assert isinstance(itr, collections.abc.AsyncGenerator)
+
+    with pytest.raises(TypeError):
+        for _ in itr:
+            pass
