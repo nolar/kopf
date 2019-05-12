359829a70ded24d4721555539127461f4447b032
Sergey Vasilyev <sergey.vasilyev@zalando.de>
2019-05-12 17:04:38 +0200

    Test the queueing and multiplexing

--- tests/reactor/conftest.py
+++ tests/reactor/conftest.py
@@ -1,7 +1,66 @@
+import asyncio
+
 import pytest
+from asynctest import CoroutineMock
+
+from kopf.reactor.queueing import watcher
+from kopf.reactor.queueing import worker as original_worker
+from kopf.reactor.watching import streaming_watch
 
 
 @pytest.fixture()
 def stream(mocker):
+    """ A mock for the stream of events as if returned by K8s client. """
     stream = mocker.patch('kubernetes.watch.Watch.stream')
     return stream
+
+
+@pytest.fixture()
+def handler():
+    """ A mock for handler -- to be checked if the handler has been called. """
+    return CoroutineMock()
+
+
+@pytest.fixture()
+def worker_spy(mocker):
+    """ Spy on the watcher: actually call it, but provide the mock-fields. """
+    spy = CoroutineMock(spec=original_worker, wraps=original_worker)
+    return mocker.patch('kopf.reactor.queueing.worker', spy)
+
+
+@pytest.fixture()
+def worker_mock(mocker):
+    """ Prevent the queue consumption, so that the queues could be checked. """
+    return mocker.patch('kopf.reactor.queueing.worker')
+
+
+@pytest.fixture()
+def watcher_limited(mocker):
+    """ Make event streaming finite, watcher exits after depletion. """
+    mocker.patch('kopf.reactor.queueing.infinite_watch', new=streaming_watch)
+
+
+@pytest.fixture()
+def watcher_in_background(resource, handler, event_loop, worker_spy, stream):
+
+    # Prevent any real streaming for the very beginning, before it even starts.
+    stream.return_value = iter([])
+
+    # Spawn a watcher in the background.
+    coro = watcher(
+        namespace=None,
+        resource=resource,
+        handler=handler,
+    )
+    task = event_loop.create_task(coro)
+
+    try:
+        # Go for a test.
+        yield task
+    finally:
+        # Terminate the watcher to cleanup the loop.
+        task.cancel()
+        try:
+            event_loop.run_until_complete(task)
+        except asyncio.CancelledError:
+            pass
---
+++ tests/reactor/test_queueing.py
@@ -0,0 +1,202 @@
+"""
+Only the tests from the watching (simulated) to the handling (substituted).
+
+Excluded: the watching-streaming routines
+(see ``tests_streaming.py`` and ``test_watching.py``).
+
+Excluded: the causation and handling routines
+(to be done later).
+
+Used for internal control that the event queueing works are intended.
+If the intentions change, the tests should be rewritten.
+They are NOT part of the public interface of the framework.
+"""
+import asyncio
+import weakref
+
+import pytest
+
+from kopf.reactor.queueing import watcher, EOS
+
+# Some overhead for the synchronous logic in async tests: it also takes time.
+CODE_OVERHEAD = 0.05  # 0.01 is too fast, 0.1 is too slow, 0.5 is good enough.
+
+
+@pytest.mark.parametrize('uids, cnts, events', [
+
+    pytest.param(['uid1'], [1], [
+        {'type': 'ADDED', 'object': {'metadata': {'uid': 'uid1'}}},
+    ], id='single'),
+
+    pytest.param(['uid1'], [3], [
+        {'type': 'ADDED', 'object': {'metadata': {'uid': 'uid1'}}},
+        {'type': 'MODIFIED', 'object': {'metadata': {'uid': 'uid1'}}},
+        {'type': 'DELETED', 'object': {'metadata': {'uid': 'uid1'}}},
+    ], id='multiple'),
+
+    pytest.param(['uid1', 'uid2'], [3, 2], [
+        {'type': 'ADDED', 'object': {'metadata': {'uid': 'uid1'}}},
+        {'type': 'ADDED', 'object': {'metadata': {'uid': 'uid2'}}},
+        {'type': 'MODIFIED', 'object': {'metadata': {'uid': 'uid1'}}},
+        {'type': 'MODIFIED', 'object': {'metadata': {'uid': 'uid2'}}},
+        {'type': 'DELETED', 'object': {'metadata': {'uid': 'uid1'}}},
+    ], id='mixed'),
+
+])
+@pytest.mark.usefixtures('watcher_limited')
+async def test_event_multiplexing(worker_mock, timer, resource, handler, stream, events, uids, cnts):
+    """ Verify that every unique uid goes into its own queue+worker, which are never shared. """
+
+    # Inject the events of unique objects - to produce few queues/workers.
+    stream.return_value = iter(events)
+
+    # Run the watcher (near-instantly and test-blocking).
+    with timer:
+        await watcher(
+            namespace=None,
+            resource=resource,
+            handler=handler,
+        )
+
+    # The queues are not cleared by the mocked worker, but the worker exits fast.
+    assert timer.seconds < CODE_OVERHEAD
+
+    # The handler must not be called by the watcher, only by the worker.
+    # But the worker (even if mocked) must be called & awaited by the watcher.
+    assert not handler.awaited
+    assert not handler.called
+    assert worker_mock.awaited
+
+    # Are the worker-queues created by the watcher? Populated as expected?
+    # One queue per unique uid? All events are sequential? EOS marker appended?
+    assert worker_mock.call_count == len(uids)
+    assert worker_mock.call_count == len(cnts)
+    for uid, cnt, (args, kwargs) in zip(uids, cnts, worker_mock.call_args_list):
+        key = kwargs['key']
+        queues = kwargs['queues']
+        assert kwargs['handler'] is handler
+        assert key == (resource, uid)
+        assert key in queues
+
+        queue_events = []
+        while not queues[key].empty():
+            queue_events.append(queues[key].get_nowait())
+
+        assert len(queue_events) == cnt + 1
+        assert queue_events[-1] is EOS
+        assert all(queue_event['object']['metadata']['uid'] == uid
+                   for queue_event in queue_events[:-1])
+
+
+@pytest.mark.parametrize('uids, vals, events', [
+
+    pytest.param(['uid1'], ['b'], [
+        {'type': 'MODIFIED', 'object': {'metadata': {'uid': 'uid1'}, 'spec': 'a'}},
+        {'type': 'MODIFIED', 'object': {'metadata': {'uid': 'uid1'}, 'spec': 'b'}},
+    ], id='the same'),
+
+    pytest.param(['uid1', 'uid2'], ['a', 'b'], [
+        {'type': 'MODIFIED', 'object': {'metadata': {'uid': 'uid1'}, 'spec': 'a'}},
+        {'type': 'MODIFIED', 'object': {'metadata': {'uid': 'uid2'}, 'spec': 'b'}},
+    ], id='distinct'),
+
+    pytest.param(['uid1', 'uid2', 'uid3'], ['e', 'd', 'f'], [
+        {'type': 'ADDED', 'object': {'metadata': {'uid': 'uid1'}, 'spec': 'a'}},
+        {'type': 'ADDED', 'object': {'metadata': {'uid': 'uid2'}, 'spec': 'b'}},
+        {'type': 'MODIFIED', 'object': {'metadata': {'uid': 'uid1'}, 'spec': 'c'}},
+        {'type': 'MODIFIED', 'object': {'metadata': {'uid': 'uid2'}, 'spec': 'd'}},
+        {'type': 'DELETED', 'object': {'metadata': {'uid': 'uid1'}, 'spec': 'e'}},
+        {'type': 'DELETED', 'object': {'metadata': {'uid': 'uid3'}, 'spec': 'f'}},
+    ], id='mixed'),
+
+])
+@pytest.mark.usefixtures('watcher_limited')
+async def test_event_batching(mocker, resource, handler, timer, stream, events, uids, vals):
+    """ Verify that only the last event per uid is actually handled. """
+
+    # Override the default timeouts to make the tests faster.
+    mocker.patch('kopf.reactor.queueing.WORKER_IDLE_TIMEOUT', 0.5)
+    mocker.patch('kopf.reactor.queueing.WORKER_BATCH_WINDOW', 0.1)
+    mocker.patch('kopf.reactor.queueing.WORKER_EXIT_TIMEOUT', 0.5)
+
+    # Inject the events of unique objects - to produce few queues/workers.
+    stream.return_value = iter(events)
+
+    # Run the watcher (near-instantly and test-blocking).
+    with timer:
+        await watcher(
+            namespace=None,
+            resource=resource,
+            handler=handler,
+        )
+
+    # Significantly less than the queue getting timeout, but sufficient to run.
+    # 2 <= 1 pull for the event chain + 1 pull for EOS. TODO: 1x must be enough.
+    from kopf.reactor.queueing import WORKER_BATCH_WINDOW
+    assert timer.seconds < WORKER_BATCH_WINDOW + CODE_OVERHEAD
+
+    # Was the handler called at all? Awaited as needed for async fns?
+    assert handler.awaited
+
+    # Was it called only once per uid? Only with the latest event?
+    assert handler.call_count == len(uids)
+    assert handler.call_count == len(vals)
+    for uid, val, (args, kwargs) in zip(uids, vals, handler.call_args_list):
+        event = kwargs['event']
+        assert event['object']['metadata']['uid'] == uid
+        assert event['object']['spec'] == val
+
+
+@pytest.mark.parametrize('unique, events', [
+
+    pytest.param(1, [
+        {'type': 'ADDED', 'object': {'metadata': {'uid': 'uid1'}}},
+        {'type': 'MODIFIED', 'object': {'metadata': {'uid': 'uid1'}}},
+        {'type': 'DELETED', 'object': {'metadata': {'uid': 'uid1'}}},
+    ], id='the same'),
+
+    pytest.param(2, [
+        {'type': 'ADDED', 'object': {'metadata': {'uid': 'uid1'}}},
+        {'type': 'ADDED', 'object': {'metadata': {'uid': 'uid2'}}},
+    ], id='distinct'),
+
+])
+@pytest.mark.usefixtures('watcher_in_background')
+async def test_garbage_collection_of_queues(mocker, stream, events, unique, worker_spy):
+
+    # Override the default timeouts to make the tests faster.
+    mocker.patch('kopf.reactor.queueing.WORKER_IDLE_TIMEOUT', 0.5)
+    mocker.patch('kopf.reactor.queueing.WORKER_BATCH_WINDOW', 0.1)
+    mocker.patch('kopf.reactor.queueing.WORKER_EXIT_TIMEOUT', 0.5)
+
+    # Inject the events of unique objects - to produce few queues/workers.
+    stream.return_value = iter(events)
+
+    # Give it a moment to populate the queues and spawn all the workers.
+    # Intercept and remember _any_ seen dict of queues for further checks.
+    while worker_spy.call_count < unique:
+        await asyncio.sleep(0.001)  # give control to the loop
+    queues = worker_spy.call_args_list[-1][1]['queues']
+
+    # The mutable(!) queues dict is now populated with the objects' queues.
+    assert len(queues) != 0  # usually 1, but can be 2+ if it is fast enough.
+
+    # Weakly remember the queues to make sure they are gc'ed later.
+    refs = [weakref.ref(queue) for queue in queues.values()]
+    assert all([ref() is not None for ref in refs])
+
+    # Give the workers some time to finish waiting for the events.
+    # Once the idle timeout, they will exit and gc their individual queues.
+    from kopf.reactor.queueing import WORKER_IDLE_TIMEOUT, WORKER_BATCH_WINDOW
+    await asyncio.sleep(WORKER_BATCH_WINDOW)  # depleting the queues.
+    await asyncio.sleep(WORKER_IDLE_TIMEOUT)  # idling on empty queues.
+    await asyncio.sleep(CODE_OVERHEAD)
+
+    # The mutable(!) queues dict is now empty, i.e. garbage-collected.
+    assert len(queues) == 0
+
+    # Truly garbage-collected? Memory freed?
+    assert all([ref() is None for ref in refs])
+
+
+# TODO: also add tests for the depletion of the workers pools on cancellation (+timing)
