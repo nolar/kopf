13eda0fea4f16611a09fc005b7ebd8f4d8b9f831
Dmitry Bazhal <bajal@tutu.ru>
2019-06-18 11:32:58 +0300

    fix patching tests

--- tests/k8s/test_patching.py
+++ tests/k8s/test_patching.py
@@ -6,16 +6,13 @@ from asynctest import call
 from kopf.k8s.patching import patch_obj
 
 
-def test_by_name_clustered(client_mock, resource):
+async def test_by_name_clustered(client_mock, resource):
     patch = object()
     apicls_mock = client_mock.CustomObjectsApi
     sidefn_mock = apicls_mock.return_value.patch_namespaced_custom_object
     mainfn_mock = apicls_mock.return_value.patch_cluster_custom_object
 
-    task = asyncio.create_task(patch_obj(resource=resource, namespace=None, name='name1', patch=patch))
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(task)
-    res = task.result
+    res = await patch_obj(resource=resource, namespace=None, name='name1', patch=patch)
     assert res is None  # never return any k8s-client specific things
 
     assert not sidefn_mock.called
@@ -29,16 +26,13 @@ def test_by_name_clustered(client_mock, resource):
     )]
 
 
-def test_by_name_namespaced(client_mock, resource):
+async def test_by_name_namespaced(client_mock, resource):
     patch = object()
     apicls_mock = client_mock.CustomObjectsApi
     sidefn_mock = apicls_mock.return_value.patch_cluster_custom_object
     mainfn_mock = apicls_mock.return_value.patch_namespaced_custom_object
 
-    task = asyncio.create_task(patch_obj(resource=resource, namespace='ns1', name='name1', patch=patch))
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(task)
-    res = task.result
+    res = await patch_obj(resource=resource, namespace='ns1', name='name1', patch=patch)
     assert res is None  # never return any k8s-client specific things
 
     assert not sidefn_mock.called
@@ -53,17 +47,14 @@ def test_by_name_namespaced(client_mock, resource):
     )]
 
 
-def test_by_body_clustered(client_mock, resource):
+async def test_by_body_clustered(client_mock, resource):
     patch = object()
     apicls_mock = client_mock.CustomObjectsApi
     sidefn_mock = apicls_mock.return_value.patch_namespaced_custom_object
     mainfn_mock = apicls_mock.return_value.patch_cluster_custom_object
 
     body = {'metadata': {'name': 'name1'}}
-    task = asyncio.create_task(patch_obj(resource=resource, body=body, patch=patch))
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(task)
-    res = task.result
+    res = await patch_obj(resource=resource, body=body, patch=patch)
     assert res is None  # never return any k8s-client specific things
 
     assert not sidefn_mock.called
@@ -102,7 +93,7 @@ def test_by_body_namespaced(client_mock, resource):
     )]
 
 
-def test_raises_when_body_conflicts_with_namespace(client_mock, resource):
+async def test_raises_when_body_conflicts_with_namespace(client_mock, resource):
     patch = object()
     apicls_mock = client_mock.CustomObjectsApi
     sidefn_mock = apicls_mock.return_value.patch_namespaced_custom_object
@@ -110,14 +101,13 @@ def test_raises_when_body_conflicts_with_namespace(client_mock, resource):
 
     body = {'metadata': {'namespace': 'ns1', 'name': 'name1'}}
     with pytest.raises(TypeError):
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(patch_obj(resource=resource, body=body, namespace='ns1', patch=patch))
+        await patch_obj(resource=resource, body=body, namespace='ns1', patch=patch)
 
     assert not sidefn_mock.called
     assert not mainfn_mock.called
 
 
-def test_raises_when_body_conflicts_with_name(client_mock, resource):
+async def test_raises_when_body_conflicts_with_name(client_mock, resource):
     patch = object()
     apicls_mock = client_mock.CustomObjectsApi
     sidefn_mock = apicls_mock.return_value.patch_namespaced_custom_object
@@ -125,13 +115,13 @@ def test_raises_when_body_conflicts_with_name(client_mock, resource):
 
     body = {'metadata': {'namespace': 'ns1', 'name': 'name1'}}
     with pytest.raises(TypeError):
-        patch_obj(resource=resource, body=body, name='name1', patch=patch)
+        await patch_obj(resource=resource, body=body, name='name1', patch=patch)
 
     assert not sidefn_mock.called
     assert not mainfn_mock.called
 
 
-def test_raises_when_body_conflicts_with_ids(client_mock, resource):
+async def test_raises_when_body_conflicts_with_ids(client_mock, resource):
     patch = object()
     apicls_mock = client_mock.CustomObjectsApi
     sidefn_mock = apicls_mock.return_value.patch_namespaced_custom_object
@@ -139,7 +129,7 @@ def test_raises_when_body_conflicts_with_ids(client_mock, resource):
 
     body = {'metadata': {'namespace': 'ns1', 'name': 'name1'}}
     with pytest.raises(TypeError):
-        patch_obj(resource=resource, body=body, namespace='ns1', name='name1', patch=patch)
+        await patch_obj(resource=resource, body=body, namespace='ns1', name='name1', patch=patch)
 
     assert not sidefn_mock.called
     assert not mainfn_mock.called
