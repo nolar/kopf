d739050be60bf1885a8cf44c65c90e40876609bb
Sergey Vasilyev <nolar@nolar.info>
2019-09-26 15:01:06 +0200

    Make the implicit "v" prefix explicit in versions

--- tools/minikube-for-travis.sh
+++ tools/minikube-for-travis.sh
@@ -3,11 +3,11 @@
 set -eu
 set -x
 
-curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v${KUBERNETES_VERSION}/bin/linux/amd64/kubectl
+curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/"${KUBERNETES_VERSION}"/bin/linux/amd64/kubectl
 chmod +x kubectl
 sudo mv kubectl /usr/local/bin/
 
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v${MINIKUBE_VERSION}/minikube-linux-amd64
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/"${MINIKUBE_VERSION}"/minikube-linux-amd64
 chmod +x minikube
 sudo mv minikube /usr/local/bin/
 
@@ -18,6 +18,6 @@ sudo minikube start \
     --vm-driver=none \
     --extra-config=apiserver.authorization-mode=RBAC \
     --extra-config=apiserver.runtime-config=events.k8s.io/v1beta1=false \
-    --kubernetes-version=v${KUBERNETES_VERSION}
+    --kubernetes-version="${KUBERNETES_VERSION}"
 
 sudo chown -R travis: /home/travis/.minikube/
