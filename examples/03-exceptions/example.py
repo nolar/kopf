afce96873455cdd226307da38d712e596b2e83d7
Sergey Vasilyev <nolar@nolar.info>
2019-11-05 17:18:46 +0100

    Improve the example of exceptions with the new features

--- examples/03-exceptions/example.py
+++ examples/03-exceptions/example.py
@@ -1,11 +1,9 @@
-import time
-
 import kopf
 
 E2E_TRACEBACKS = True
-E2E_CREATION_STOP_WORDS = ['Third failure, the final one']
-E2E_SUCCESS_COUNTS = {}
-E2E_FAILURE_COUNTS = {'create_fn': 1}
+E2E_CREATION_STOP_WORDS = ['Something has changed,']
+E2E_SUCCESS_COUNTS = {'eventual_success_with_few_messages': 1}
+E2E_FAILURE_COUNTS = {'eventual_failure_with_tracebacks': 1, 'instant_failure_with_traceback': 1, 'instant_failure_with_only_a_message': 1}
 
 
 class MyException(Exception):
@@ -13,11 +11,21 @@ class MyException(Exception):
 
 
 @kopf.on.create('zalando.org', 'v1', 'kopfexamples')
-def create_fn(retry, **kwargs):
-    time.sleep(0.1)  # for different timestamps of the events
-    if not retry:
-        raise kopf.TemporaryError("First failure.", delay=1)
-    elif retry == 1:
-        raise MyException("Second failure.")
-    else:
-        raise kopf.PermanentError("Third failure, the final one.")
+def instant_failure_with_only_a_message(**kwargs):
+    raise kopf.PermanentError("Fail once and for all.")
+
+
+@kopf.on.create('zalando.org', 'v1', 'kopfexamples')
+def eventual_success_with_few_messages(retry, **kwargs):
+    if retry < 3:  # 0, 1, 2, 3
+        raise kopf.TemporaryError("Expected recoverable error.", delay=1.0)
+
+
+@kopf.on.create('zalando.org', 'v1', 'kopfexamples', retries=3, cooldown=1.0)
+def eventual_failure_with_tracebacks(**kwargs):
+    raise MyException("An error that is supposed to be recoverable.")
+
+
+@kopf.on.create('zalando.org', 'v1', 'kopfexamples', errors=kopf.ErrorsMode.PERMANENT, cooldown=1.0)
+def instant_failure_with_traceback(**kwargs):
+    raise MyException("An error that is supposed to be recoverable.")
