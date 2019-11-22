76bc3dc51b6bc20b025a06de93d41a37ec266cb7
Sergey Vasilyev <nolar@nolar.info>
2019-11-21 20:39:17 -0800

    Add bi-directional toggles as synchronisation primitives

--- kopf/structs/primitives.py
+++ kopf/structs/primitives.py
@@ -55,3 +55,67 @@ async def raise_flag(
         flag.set()
     else:
         raise TypeError(f"Unsupported type of a flag: {flag!r}")
+
+
+class Toggle:
+    """
+    An synchronisation primitive that can be awaited both until set or cleared.
+
+    For one-directional toggles, `asyncio.Event` is sufficient.
+    But these events cannot be awaited until cleared.
+
+    The bi-directional toggles are needed in some places in the code, such as
+    in the population/depletion of a `Vault`, or as an operator's freeze-mode.
+    """
+
+    def __init__(
+            self,
+            __val: bool = False,
+            *,
+            loop: Optional[asyncio.AbstractEventLoop] = None,
+    ) -> None:
+        super().__init__()
+        self._condition = asyncio.Condition(loop=loop)
+        self._state: bool = bool(__val)
+
+    @property
+    def loop(self) -> asyncio.AbstractEventLoop:
+        return self._condition._loop
+
+    def __bool__(self) -> bool:
+        """
+        In the boolean context, a toggle evaluates to its current on/off state.
+
+        An equivalent of `.is_on` / `.is_off`.
+        """
+        return bool(self._state)
+
+    def is_on(self) -> bool:
+        """ Check if the toggle is currently on (opposite of `.is_off`). """
+        return bool(self._state)
+
+    def is_off(self) -> bool:
+        """ Check if the toggle is currently off (opposite of `.is_on`). """
+        return bool(not self._state)
+
+    async def turn_on(self) -> None:
+        """ Turn the toggle on, and wake up the tasks waiting for that. """
+        async with self._condition:
+            self._state = True
+            self._condition.notify_all()
+
+    async def turn_off(self) -> None:
+        """ Turn the toggle off, and wake up the tasks waiting for that. """
+        async with self._condition:
+            self._state = False
+            self._condition.notify_all()
+
+    async def wait_for_on(self) -> None:
+        """ Wait until the toggle is turned on (if not yet). """
+        async with self._condition:
+            await self._condition.wait_for(lambda: self._state)
+
+    async def wait_for_off(self) -> None:
+        """ Wait until the toggle is turned off (if not yet). """
+        async with self._condition:
+            await self._condition.wait_for(lambda: not self._state)
