194c33c82aa8bcc0694422430249eb9e324b79cd
Sergey Vasilyev <nolar@nolar.info>
2020-03-12 18:25:46 +0100

    Resolve the legacy registries' typing issues through a frankenclass
    These classes and modules are deprecated anyway, so there is no neeed
    to keep them in a nice state, and make it the right way. Only minimally
    sufficient fix is enough to make it both work and pass the type-checks.

--- kopf/toolkits/legacy_registries.py
+++ kopf/toolkits/legacy_registries.py
@@ -7,17 +7,31 @@ with an incompatible class hierarchy and method signatures.
 """
 import abc
 import warnings
-from typing import Any, Union, Sequence, Iterator
+from typing import Any, Union, Sequence, Iterator, Optional
 
 from kopf.reactor import causation
+from kopf.reactor import errors as errors_
 from kopf.reactor import handlers
 from kopf.reactor import registries
 from kopf.structs import bodies
 from kopf.structs import callbacks
+from kopf.structs import dicts
+from kopf.structs import filters
 from kopf.structs import patches
 from kopf.structs import resources as resources_
 
 AnyCause = Union[causation.ResourceWatchingCause, causation.ResourceChangingCause]
+AnyHandler = Union[handlers.ResourceWatchingHandler, handlers.ResourceChangingHandler]
+AnyHandlerFn = Union[callbacks.ResourceWatchingFn, callbacks.ResourceChangingFn]
+
+
+# A frankenclass to match both watching- & changing handlers for signatures & typing.
+# An equivalent of the all-purpose ResourceHandler before it was split to specialised sub-classes.
+class LegacyAllPurposeResourcerHandler(
+    handlers.ResourceChangingHandler,
+    handlers.ResourceWatchingHandler,
+):
+    fn: AnyHandlerFn  # type: ignore
 
 
 class BaseRegistry(metaclass=abc.ABCMeta):
@@ -32,14 +46,14 @@ class BaseRegistry(metaclass=abc.ABCMeta):
             self,
             resource: resources_.Resource,
             event: bodies.RawEvent,
-    ) -> Sequence[handlers.ResourceHandler]:
+    ) -> Sequence[handlers.ResourceWatchingHandler]:
         raise NotImplementedError
 
     @abc.abstractmethod
     def get_cause_handlers(
             self,
             cause: causation.ResourceChangingCause,
-    ) -> Sequence[handlers.ResourceHandler]:
+    ) -> Sequence[handlers.ResourceChangingHandler]:
         raise NotImplementedError
 
     @abc.abstractmethod
@@ -47,40 +61,79 @@ class BaseRegistry(metaclass=abc.ABCMeta):
             self,
             resource: resources_.Resource,
             event: bodies.RawEvent,
-    ) -> Iterator[handlers.ResourceHandler]:
+    ) -> Iterator[handlers.ResourceWatchingHandler]:
         raise NotImplementedError
 
     @abc.abstractmethod
     def iter_cause_handlers(
             self,
             cause: causation.ResourceChangingCause,
-    ) -> Iterator[handlers.ResourceHandler]:
+    ) -> Iterator[handlers.ResourceChangingHandler]:
         raise NotImplementedError
 
 
 class SimpleRegistry(BaseRegistry, registries.ResourceRegistry[
         AnyCause,
-        Union[callbacks.ResourceWatchingFn, callbacks.ResourceChangingFn],
-        handlers.ResourceHandler]):
+        AnyHandlerFn,
+        AnyHandler]):
     """
     .. deprecated: 1.0
 
         Replaced with `ResourceWatchingRegistry` and `ResourceChangingRegistry`.
     """
 
+    def register(
+            self,
+            fn: AnyHandlerFn,
+            *,
+            id: Optional[str] = None,
+            reason: Optional[causation.Reason] = None,
+            event: Optional[str] = None,  # deprecated, use `reason`
+            field: Optional[dicts.FieldSpec] = None,
+            errors: Optional[errors_.ErrorsMode] = None,
+            timeout: Optional[float] = None,
+            retries: Optional[int] = None,
+            backoff: Optional[float] = None,
+            cooldown: Optional[float] = None,  # deprecated, use `backoff`
+            initial: Optional[bool] = None,
+            deleted: Optional[bool] = None,
+            requires_finalizer: bool = False,
+            labels: Optional[filters.MetaFilter] = None,
+            annotations: Optional[filters.MetaFilter] = None,
+            when: Optional[callbacks.WhenFilterFn] = None,
+    ) -> AnyHandlerFn:
+        warnings.warn("registry.register() is deprecated; "
+                      "use @kopf.on... decorators with registry= kwarg.",
+                      DeprecationWarning)
+
+        if reason is None and event is not None:
+            reason = causation.Reason(event)
+
+        real_field = dicts.parse_field(field) or None  # to not store tuple() as a no-field case.
+        real_id = registries.generate_id(fn=fn, id=id, suffix=".".join(real_field or []))
+        handler = LegacyAllPurposeResourcerHandler(
+            id=real_id, fn=fn,  # type: ignore
+            reason=reason, field=real_field,
+            errors=errors, timeout=timeout, retries=retries, backoff=backoff, cooldown=cooldown,
+            initial=initial, deleted=deleted, requires_finalizer=requires_finalizer,
+            labels=labels, annotations=annotations, when=when,
+        )
+        self.append(handler)
+        return fn
+
     # A dummy to avoid ABCMeta restrictions, and for rudimentary class testing.
     # Yield/return everything unconditionally -- it was not used previously anyway.
     def iter_handlers(
             self,
             cause: AnyCause,
-    ) -> Iterator[handlers.ResourceHandler]:
+    ) -> Iterator[AnyHandler]:
         yield from self._handlers
 
     def get_event_handlers(
             self,
             resource: resources_.Resource,
             event: bodies.RawEvent,
-    ) -> Sequence[handlers.ResourceHandler]:
+    ) -> Sequence[handlers.ResourceWatchingHandler]:
         warnings.warn("SimpleRegistry.get_event_handlers() is deprecated; use "
                       "ResourceWatchingRegistry.get_handlers().", DeprecationWarning)
         return list(registries._deduplicated(self.iter_event_handlers(
@@ -89,7 +142,7 @@ class SimpleRegistry(BaseRegistry, registries.ResourceRegistry[
     def get_cause_handlers(
             self,
             cause: causation.ResourceChangingCause,
-    ) -> Sequence[handlers.ResourceHandler]:
+    ) -> Sequence[handlers.ResourceChangingHandler]:
         warnings.warn("SimpleRegistry.get_cause_handlers() is deprecated; use "
                       "ResourceChangingRegistry.get_handlers().", DeprecationWarning)
         return list(registries._deduplicated(self.iter_cause_handlers(cause=cause)))
@@ -98,25 +151,29 @@ class SimpleRegistry(BaseRegistry, registries.ResourceRegistry[
             self,
             resource: resources_.Resource,
             event: bodies.RawEvent,
-    ) -> Iterator[handlers.ResourceHandler]:
+    ) -> Iterator[handlers.ResourceWatchingHandler]:
         warnings.warn("SimpleRegistry.iter_event_handlers() is deprecated; use "
                       "ResourceWatchingRegistry.iter_handlers().", DeprecationWarning)
 
         cause = _create_watching_cause(resource, event)
         for handler in self._handlers:
-            if registries.match(handler=handler, cause=cause, ignore_fields=True):
+            if not isinstance(handler, handlers.ResourceWatchingHandler):
+                pass
+            elif registries.match(handler=handler, cause=cause, ignore_fields=True):
                 yield handler
 
     def iter_cause_handlers(
             self,
             cause: causation.ResourceChangingCause,
-    ) -> Iterator[handlers.ResourceHandler]:
+    ) -> Iterator[handlers.ResourceChangingHandler]:
         warnings.warn("SimpleRegistry.iter_cause_handlers() is deprecated; use "
                       "ResourceChangingRegistry.iter_handlers().", DeprecationWarning)
 
         changed_fields = frozenset(field for _, field, _, _ in cause.diff or [])
         for handler in self._handlers:
-            if handler.reason is None or handler.reason == cause.reason:
+            if not isinstance(handler, handlers.ResourceChangingHandler):
+                pass
+            elif handler.reason is None or handler.reason == cause.reason:
                 if handler.initial and not cause.initial:
                     pass  # ignore initial handlers in non-initial causes.
                 elif registries.match(handler=handler, cause=cause,
@@ -155,7 +212,7 @@ class GlobalRegistry(BaseRegistry, registries.OperatorRegistry):
             self,
             resource: resources_.Resource,
             event: bodies.RawEvent,
-    ) -> Sequence[handlers.ResourceHandler]:
+    ) -> Sequence[handlers.ResourceWatchingHandler]:
         warnings.warn("GlobalRegistry.get_event_handlers() is deprecated; use "
                       "OperatorRegistry.get_resource_watching_handlers().", DeprecationWarning)
         cause = _create_watching_cause(resource=resource, event=event)
@@ -164,7 +221,7 @@ class GlobalRegistry(BaseRegistry, registries.OperatorRegistry):
     def get_cause_handlers(
             self,
             cause: causation.ResourceChangingCause,
-    ) -> Sequence[handlers.ResourceHandler]:
+    ) -> Sequence[handlers.ResourceChangingHandler]:
         warnings.warn("GlobalRegistry.get_cause_handlers() is deprecated; use "
                       "OperatorRegistry.get_resource_changing_handlers().", DeprecationWarning)
         return self.get_resource_changing_handlers(cause=cause)
@@ -173,7 +230,7 @@ class GlobalRegistry(BaseRegistry, registries.OperatorRegistry):
             self,
             resource: resources_.Resource,
             event: bodies.RawEvent,
-    ) -> Iterator[handlers.ResourceHandler]:
+    ) -> Iterator[handlers.ResourceWatchingHandler]:
         warnings.warn("GlobalRegistry.iter_event_handlers() is deprecated; use "
                       "OperatorRegistry.iter_resource_watching_handlers().", DeprecationWarning)
         cause = _create_watching_cause(resource=resource, event=event)
@@ -182,7 +239,7 @@ class GlobalRegistry(BaseRegistry, registries.OperatorRegistry):
     def iter_cause_handlers(
             self,
             cause: causation.ResourceChangingCause,
-    ) -> Iterator[handlers.ResourceHandler]:
+    ) -> Iterator[handlers.ResourceChangingHandler]:
         warnings.warn("GlobalRegistry.iter_cause_handlers() is deprecated; use "
                       "OperatorRegistry.iter_resource_changing_handlers().", DeprecationWarning)
         yield from self.iter_resource_changing_handlers(cause=cause)
