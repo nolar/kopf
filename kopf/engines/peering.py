b5e529d67b6e39a1fa38cafd3db2801a1003f149
Sergey Vasilyev <nolar@nolar.info>
2020-10-21 12:04:43 +0200

    Ignore IPv6 ARPA hostnames for peering identities
    With IPv6, the peering operators often got the identities like `1.0.0.0...0.0.ip6.arpa`, which does not make it easier to identify the pod where the operator is running. This is because of how `socket.getfqdn()` works — it does not exclude IPv6 pseudo-hostnames properly.
    
    With this fix, the operators' identities in IPv6 environments will return back to normal pod names.

--- kopf/engines/peering.py
+++ kopf/engines/peering.py
@@ -35,13 +35,13 @@ import getpass
 import logging
 import os
 import random
-import socket
 from typing import Any, Dict, Iterable, Mapping, NoReturn, Optional, Union, cast
 
 import iso8601
 
 from kopf.clients import fetching, patching
 from kopf.structs import bodies, patches, primitives, resources
+from kopf.utilities import hostnames
 
 logger = logging.getLogger(__name__)
 
@@ -298,7 +298,7 @@ def detect_own_id() -> str:
         return pod
 
     user = getpass.getuser()
-    host = socket.getfqdn()
+    host = hostnames.get_descriptive_hostname()
     now = datetime.datetime.utcnow().isoformat()
     rnd = ''.join(random.choices('abcdefhijklmnopqrstuvwxyz0123456789', k=6))
     return f'{user}@{host}/{now}/{rnd}'
--- tests/peering/test_id_generation.py
+++ tests/peering/test_id_generation.py
@@ -5,21 +5,84 @@ import pytest
 
 from kopf.engines.peering import detect_own_id
 
+SAME_GOOD = [
+    ('some-host.example.com', 'other-host.example.com'),
+    ('some-host', 'other-host'),
+]
+
+# Priorities: A (left) should be selected over B (right).
+GOOD_BAD = [
+    ('some-host.example.com', 'some-host'),
+    ('some-host.example.com', '1.2.3.4'),
+    ('some-host.example.com', '::1'),
+    ('some-host.example.com', '1.0...0.ip6.arpa'),
+    ('some-host.example.com', '4.3.2.1.in-addr.arpa'),
+    ('some-host.example.com', '1.2.3.4'),
+    ('some-host', '1.2.3.4'),
+    ('some-host', '::1'),
+    ('some-host', '1.0...0.ip6.arpa'),
+    ('some-host', '4.3.2.1.in-addr.arpa'),
+    ('some-host', '1.2.3.4'),
+]
+
 
 @pytest.fixture(autouse=True)
 def _intercept_os_calls(mocker):
     mocker.patch('getpass.getuser', return_value='some-user')
-    mocker.patch('socket.getfqdn', return_value='some-host')
     mocker.patch('random.choices', return_value='random-str')
+    mocker.patch('socket.gethostname')
+    mocker.patch('socket.gethostbyaddr')
 
 
 def test_from_a_pod_id(mocker):
+    mocker.patch('socket.gethostname', return_value='some-host')
+    mocker.patch('socket.gethostbyaddr', side_effect=lambda fqdn: (fqdn, [], []))
     mocker.patch.dict(os.environ, POD_ID='some-pod-1')
     own_id = detect_own_id()
     assert own_id == 'some-pod-1'
 
 
 @freezegun.freeze_time('2020-12-31T23:59:59.123456')
-def test_with_defaults():
+@pytest.mark.parametrize('good1, good2', SAME_GOOD)
+def test_good_hostnames_over_good_aliases__symmetric(mocker, good1, good2):
+    mocker.patch('socket.gethostname', return_value=good1)
+    mocker.patch('socket.gethostbyaddr', side_effect=lambda fqdn: (fqdn, [good2], []))
     own_id = detect_own_id()
-    assert own_id == 'some-user@some-host/2020-12-31T23:59:59.123456/random-str'
+    assert own_id == f'some-user@{good1}/2020-12-31T23:59:59.123456/random-str'
+
+    mocker.patch('socket.gethostname', return_value=good2)
+    mocker.patch('socket.gethostbyaddr', side_effect=lambda fqdn: (fqdn, [good1], []))
+    own_id = detect_own_id()
+    assert own_id == f'some-user@{good2}/2020-12-31T23:59:59.123456/random-str'
+
+
+@freezegun.freeze_time('2020-12-31T23:59:59.123456')
+@pytest.mark.parametrize('good1, good2', SAME_GOOD)
+def test_good_aliases_over_good_addresses__symmetric(mocker, good1, good2):
+    mocker.patch('socket.gethostname', return_value='localhost')
+    mocker.patch('socket.gethostbyaddr', side_effect=lambda fqdn: (fqdn, [good1], [good2]))
+    own_id = detect_own_id()
+    assert own_id == f'some-user@{good1}/2020-12-31T23:59:59.123456/random-str'
+
+    mocker.patch('socket.gethostname', return_value='localhost')
+    mocker.patch('socket.gethostbyaddr', side_effect=lambda fqdn: (fqdn, [good2], [good1]))
+    own_id = detect_own_id()
+    assert own_id == f'some-user@{good2}/2020-12-31T23:59:59.123456/random-str'
+
+
+@freezegun.freeze_time('2020-12-31T23:59:59.123456')
+@pytest.mark.parametrize('good, bad', GOOD_BAD)
+def test_good_aliases_over_bad_hostnames(mocker, good, bad):
+    mocker.patch('socket.gethostname', return_value=bad)
+    mocker.patch('socket.gethostbyaddr', side_effect=lambda fqdn: (fqdn, [good], []))
+    own_id = detect_own_id()
+    assert own_id == f'some-user@{good}/2020-12-31T23:59:59.123456/random-str'
+
+
+@freezegun.freeze_time('2020-12-31T23:59:59.123456')
+@pytest.mark.parametrize('good, bad', GOOD_BAD)
+def test_good_addresses_over_bad_aliases(mocker, good, bad):
+    mocker.patch('socket.gethostname', return_value='localhost')
+    mocker.patch('socket.gethostbyaddr', side_effect=lambda fqdn: (fqdn, [bad], [good]))
+    own_id = detect_own_id()
+    assert own_id == f'some-user@{good}/2020-12-31T23:59:59.123456/random-str'
