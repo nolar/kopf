14b8e59648c940461a471d0f0ead638733c43df4
Sergey Vasilyev <nolar@nolar.info>
2021-03-24 22:24:10 +0100

    Introduce a primitive for a container with change notifications
    Signed-off-by: Sergey Vasilyev <nolar@nolar.info>

--- kopf/structs/primitives.py
+++ kopf/structs/primitives.py
@@ -7,7 +7,8 @@ import concurrent.futures
 import enum
 import threading
 import time
-from typing import Any, Callable, Collection, Iterable, Iterator, Optional, Set, Union
+from typing import Any, AsyncIterator, Callable, Collection, Generic, \
+                   Iterable, Iterator, Optional, Set, TypeVar, Union
 
 from kopf.utilities import aiotasks
 
@@ -98,6 +99,50 @@ async def condition_chain(
             await source.wait()
             async with target:
                 target.notify_all()
+
+
+_T = TypeVar('_T')
+
+
+class Container(Generic[_T]):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.changed = asyncio.Condition()
+        self._values: Collection[_T] = []  # 0..1 item
+
+    def get_nowait(self) -> _T:  # used mostly in testing
+        try:
+            return next(iter(self._values))
+        except StopIteration:
+            raise LookupError("No value is stored in the container.") from None
+
+    async def set(self, value: _T) -> None:
+        async with self.changed:
+            self._values = [value]
+            self.changed.notify_all()
+
+    async def wait(self) -> _T:
+        async with self.changed:
+            await self.changed.wait_for(lambda: self._values)
+        try:
+            return next(iter(self._values))
+        except StopIteration:  # impossible because of the condition's predicate
+            raise LookupError("No value is stored in the container.") from None
+
+    async def reset(self) -> None:
+        async with self.changed:
+            self._values = []
+            self.changed.notify_all()
+
+    async def as_changed(self) -> AsyncIterator[_T]:
+        async with self.changed:
+            while True:
+                try:
+                    yield next(iter(self._values))
+                except StopIteration:
+                    pass
+                await self.changed.wait()
 
 
 # Mind the value: it can be bool-evaluatable but non-bool -- always convert it.
