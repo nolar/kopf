67a8023fa0f2d1207aa9e3f71d5f9bd80435d4c2
Sergey Vasilyev <nolar@nolar.info>
2023-10-08 22:29:36 +0200

    Drop Python 3.7 (end-of-life), require at least 3.8
    Signed-off-by: Sergey Vasilyev <nolar@nolar.info>

--- DEVELOPMENT.md
+++ DEVELOPMENT.md
@@ -82,7 +82,7 @@ If you use PyCharm, create a Run/Debug Configuration as follows:
 * Mode: `module name`
 * Module name: `kopf`
 * Arguments: `run examples/01-minimal/example.py --verbose`
-* Python Interpreter: anything with Python>=3.7
+* Python Interpreter: anything with Python>=3.8
 
 Stop the console operator, and start the IDE debug session.
 Put a breakpoint in the used operator script on the first line of the function.
--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
@@ -53,7 +52,7 @@ setup(
         ],
     },
 
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     setup_requires=[
         'setuptools_scm',
     ],
