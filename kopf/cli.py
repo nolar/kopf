8093937c409e7f106a92dcecec52076010f3fca2
Sergey Vasilyev <nolar@nolar.info>
2025-05-12 09:48:02 +0200

    Suppress the type warnings for Click>=8.2.0 for CLI choices
    Signed-off-by: Sergey Vasilyev <nolar@nolar.info>

--- kopf/cli.py
+++ kopf/cli.py
@@ -29,7 +29,10 @@ class CLIControls:
     loop: Optional[asyncio.AbstractEventLoop] = None
 
 
-class LogFormatParamType(click.Choice):
+# With Click>=8.2.0, that should be `click.Choice[LogFormat]`, but it is good for now, too.
+# TODO: when Python 3.9 is dropped, upgrade dependencies to click>=8.2.0, and remake the class here.
+#       see: https://github.com/nolar/kopf/pull/1174
+class LogFormatParamType(click.Choice):  # type: ignore
 
     def __init__(self) -> None:
         super().__init__(choices=[v.name.lower() for v in loggers.LogFormat])
