6cf6d6730f087ffdd1865c3caa9c41a0ceda573d
f.medini <f.medini@nyris.io>
2025-12-13 17:23:43 +0100

    Move ignored_fields implementation to parent class DiffBaseStorage
    Signed-off-by: f.medini <f.medini@nyris.io>

--- kopf/_cogs/configs/diffbase.py
+++ kopf/_cogs/configs/diffbase.py
@@ -31,6 +31,7 @@ class DiffBaseStorage(conventions.StorageKeyMarkingConvention,
             *,
             body: bodies.Body,
             extra_fields: Iterable[dicts.FieldSpec] | None = None,
+            ignored_fields: Iterable[dicts.FieldSpec] | None = None,
     ) -> bodies.BodyEssence:
         """
         Extract only the relevant fields for the state comparisons.
@@ -83,6 +84,16 @@ class DiffBaseStorage(conventions.StorageKeyMarkingConvention,
         dicts.cherrypick(src=body, dst=essence, fields=extra_fields, picker=copy.deepcopy)
 
         self.remove_empty_stanzas(cast(bodies.BodyEssence, essence))
+
+        # Remove ignored fields if specified
+        if ignored_fields:
+            for ignored_field in ignored_fields:
+                try:
+                    dicts.remove(essence, ignored_field)
+                except TypeError:
+                    # If the field is not present or does not support item deletion, just skip it.
+                    pass
+
         return cast(bodies.BodyEssence, essence)
 
     @abc.abstractmethod
@@ -111,12 +122,12 @@ class AnnotationsDiffBaseStorage(conventions.StorageKeyFormingConvention, DiffBa
             *,
             prefix: str = 'kopf.zalando.org',
             key: str = 'last-handled-configuration',
-            ignored_fields: Iterable[dicts.FieldSpec] = None,
+            ignored_fields: Iterable[dicts.FieldSpec] | None = None,
             v1: bool = True,  # will be switched to False a few releases later
     ) -> None:
         super().__init__(prefix=prefix, v1=v1)
         self.key = key
-        self._ignored_fields = ignored_fields if ignored_fields else []
+        self._ignored_fields = ignored_fields
 
     def build(
             self,
@@ -124,15 +135,11 @@ class AnnotationsDiffBaseStorage(conventions.StorageKeyFormingConvention, DiffBa
             body: bodies.Body,
             extra_fields: Iterable[dicts.FieldSpec] | None = None,
     ) -> bodies.BodyEssence:
-        essence = super().build(body=body, extra_fields=extra_fields)
-
-        # Remove ignored fields if specified
-        for f in self._ignored_fields:
-            try:
-                dicts.remove(essence, f)
-            except TypeError:
-                # If the field is not present or does not support item deletion, just skip it.
-                pass
+        essence = super().build(
+            body=body,
+            extra_fields=extra_fields,
+            ignored_fields=self._ignored_fields
+        )
 
         self.remove_annotations(essence, set(self.make_keys(self.key, body=body)))
         self.remove_empty_stanzas(essence)
@@ -171,13 +178,13 @@ class StatusDiffBaseStorage(DiffBaseStorage):
             *,
             name: str = 'kopf',
             field: dicts.FieldSpec = 'status.{name}.last-handled-configuration',
-            ignored_fields: Iterable[dicts.FieldSpec] = None,
+            ignored_fields: Iterable[dicts.FieldSpec] | None = None,
     ) -> None:
         super().__init__()
         self._name = name
         real_field = field.format(name=self._name) if isinstance(field, str) else field
         self._field = dicts.parse_field(real_field)
-        self._ignored_fields = ignored_fields if ignored_fields else []
+        self._ignored_fields = ignored_fields
 
     @property
     def field(self) -> dicts.FieldPath:
@@ -194,15 +201,11 @@ class StatusDiffBaseStorage(DiffBaseStorage):
             body: bodies.Body,
             extra_fields: Iterable[dicts.FieldSpec] | None = None,
     ) -> bodies.BodyEssence:
-        essence = super().build(body=body, extra_fields=extra_fields)
-
-        # Remove ignored fields if specified
-        for f in self._ignored_fields:
-            try:
-                dicts.remove(essence, f)
-            except TypeError:
-                # If the field is not present or does not support item deletion, just skip it.
-                pass
+        essence = super().build(
+            body=body,
+            extra_fields=extra_fields,
+            ignored_fields=self._ignored_fields
+        )
 
         # Work around an issue with mypy not treating TypedDicts as MutableMappings.
         essence_dict = cast(dict[Any, Any], essence)
