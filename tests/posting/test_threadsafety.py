nolar/kopf

commit 81e6a33e3163d0d036c00f0e326c4524313328bc

Skip waiting for the event enqueueing
The original issue is #1212 — Kopf hangs in some situation when used via `KopfRunner` (maybe not limited to that), seemingly blocked at the log event handling, specifically in the `posting.py::K8sPoster.emit()`, specifically in `posting.py::enqueue()`, and even more specifically — after successfully scheduling the `queue.put()` coroutine, but on waiting for the `future.result()` from it. The exact cause of this is not found, but it reproduces in ≈50% of runs. The issue disappears when async kopf handlers are used (thus removing the thread pools), or when event posting is disabled (no queueing happens).

The replacing code is functionally equivalent: the fire-and-forget approach to putting an event into the queue that was & is consumed and processed elsewhere in the target event loop (the putting can happen in the thread pool executor's threads outside of the loop).

The alternatives considered:

- Removing the `future.result()`: that would be equivalent, but we miss the potential errors from queue putting (which barely ever happen, next to impossible).

- Using `loop.call_soon_threadsafe(queue.put_nowait, event)` is almost equivalent, but the errors from the (impossible) queue putting are suppressed.

All options are fine for this purpose, but only one of them is enough.

There are no tests for the bug since the root cause is not found, and it is difficult to reproduce stably (i.e. in 100% of cases).

Signed-off-by: Sergey Vasilyev <nolar@nolar.info>

kopf/_core/engines/posting.py
@@ -82,9 +82,15 @@ def enqueue(
         queue.put_nowait(event)
     else:
         # No event-loop or another event-loop - assume another thread.
-        # Use the cross-thread thread-safe methods. Block until enqueued there.
-        future = asyncio.run_coroutine_threadsafe(queue.put(event), loop=loop)
-        future.result()  # block, wait, re-raise.
+        # Use the cross-thread thread-safe methods. Do not block or wait.
+        # Beware of #1212: `run_coroutine_threadsafe(queue.put(…), loop=loop)` is flawed.
+        queue.put_nowait(event)
+        loop.call_soon_threadsafe(_no_op_event_loop_awakener)
+
+
+# The same as `lambda: None`, but with no closure data attached & better for JIT in PyPy.
+def _no_op_event_loop_awakener() -> None:
+    pass
 
 
 def event(
@@ -186,7 +192,7 @@ class K8sPoster(logging.Handler):
     """
     if sys.version_info[:2] < (3, 13):
         # Disable this optimisation for Python >= 3.13.
-        # The `handle` no longer support having `None` as lock.
+        # The `handle` no longer supports having `None` as lock.
         def createLock(self) -> None:
             # Save some time on unneeded locks. Events are posted in the background.
             # We only put events to the queue, which is already lock-protected.

commit c05e2638904f4460422b3b002b61f1b9b9f84e51

Improve thread safety in debug mode of event loops
Event loops are extra paranoid about thread safety, so they raise an error when the `kopf.event()` is called from the thread pool, i.e. sync handlers:

```python
RuntimeError("Non-thread-safe operation invoked on an event loop other than the current one")
```

This only happens if the debug mode is enabled — either explicitly via `loop.set_debug(True)` or `asyncio.run(…, debug=True)`, or implicitly with the env var `PYTHONASYNCIODEBUG=1`.

Commit 81e6a33e3163d0d036c00f0e326c4524313328bc introduced this issue, with a note that the choice between these two methods was unclear and they were the same. Turns out, they are not precisely the same.

Signed-off-by: Sergey Vasilyev <nolar@nolar.info>

kopf/_core/engines/posting.py
@@ -85,13 +85,7 @@ def enqueue(
         # No event-loop or another event-loop - assume another thread.
         # Use the cross-thread thread-safe methods. Do not block or wait.
         # Beware of #1212: `run_coroutine_threadsafe(queue.put(…), loop=loop)` is flawed.
-        queue.put_nowait(event)
-        loop.call_soon_threadsafe(_no_op_event_loop_awakener)
-
-
-# The same as `lambda: None`, but with no closure data attached & better for JIT in PyPy.
-def _no_op_event_loop_awakener() -> None:
-    pass
+        loop.call_soon_threadsafe(queue.put_nowait, event)
 
 
 def event(

tests/posting/test_threadsafety.py
@@ -95,6 +95,7 @@ async def test_nonthreadsafe_indeed_fails(chronometer, threader, event_queue):
         threader(0.2, thread_fn)
         await event_queue.get()
 
+    # We wake up on the wakeup call (0.5) instead of the proper queue.put (0.2).
     assert 0.5 <= chronometer.seconds < 0.6
     assert 0.5 <= loopometer.seconds < 0.6
     assert thread_was_called.is_set()
@@ -114,6 +115,7 @@ async def test_threadsafe_indeed_works(chronometer, threader, event_queue):
         threader(0.2, thread_fn)
         await event_queue.get()
 
+    # We wake up on time of the queue.put (0.2), not on the wakeup call (0.5).
     assert 0.2 <= chronometer.seconds < 0.3
     assert 0.2 <= loopometer.seconds < 0.3
     assert thread_was_called.is_set()
@@ -121,9 +123,15 @@ async def test_threadsafe_indeed_works(chronometer, threader, event_queue):
 
 @pytest.mark.looptime(False)
 @pytest.mark.usefixtures('event_queue_loop', 'settings_via_contextvar')
-async def test_queueing_is_threadsafe(chronometer, threader, event_queue):
+@pytest.mark.parametrize('debug', [False, True], ids=['regular', 'paranoid'])
+async def test_queueing_is_threadsafe(chronometer, threader, event_queue, debug):
     loop = asyncio.get_running_loop()
     thread_was_called = threading.Event()
+
+    # Setup the worst case to catch inconsistencies in cross-thread coordination.
+    # Debug mode makes event loops extra paranoid about thread safety, like this:
+    # RuntimeError("Non-thread-safe operation invoked on an event loop other than the current one")
+    loop.set_debug(debug)
 
     def thread_fn():
         thread_was_called.set()
@@ -134,6 +142,7 @@ async def test_queueing_is_threadsafe(chronometer, threader, event_queue):
         threader(0.2, thread_fn)
         await event_queue.get()
 
+    # We wake up on time of the queue.put (0.2), not on the wakeup call (0.5).
     assert 0.2 <= chronometer.seconds < 0.3
     assert 0.2 <= loopometer.seconds < 0.3
     assert thread_was_called.is_set()